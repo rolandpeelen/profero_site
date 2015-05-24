a032d4e515d7d39d78571dc292cf37da2b6aab80
RWJ Peelen <rolandpeelen@RWJs-MacBook.local>
2015-05-24 13:27:26 +0200

    Updates, mainly projectcontent but also small stuff in the partners section

--- src/js/services/ProjectContstant.js
+++ src/js/services/ProjectContstant.js
@@ -17,8 +17,7 @@ angular.module('proferoSite')
         name: 'SlimFitted',
         descriptionSmall: 'An online dress shirt product configurator, enabling clients to configure their next tailored shirt.',
         descriptionLarge: 'In 2014 we teamed up with the people at SlimFitted to work on a revolutionary product configurator. Instead of using photographs to ' +
-        'show their customers the shirt they configured, we used 3d visualisations based on a 3d model. Doing so provided the required flexibility to build a ' +
-        'future-proof online product configurator.',
+        'show their customers the shirt they configured, we used 3d visualisations based on a 3d model. Which enables Slimfitted to present their new models and features without having to rebuild the Configurator completely anew. ',
         tagline: 'Developing the next generation of online apps',
         partners: 'Peppr',
         categories: 'Software, Design',
@@ -28,7 +27,7 @@ angular.module('proferoSite')
       3: {
         name: 'Bachkoor Booking',
         descriptionSmall: 'A realtime booking system showing you the real availability of seats.',
-        descriptionLarge: 'In 2015 \'Bachkoor Nijmegen\' asked us to built a new website to support a more youthful image. We not only helped them with their website,' +
+        descriptionLarge: 'In 2015 \'Bachkoor Nijmegen\' asked us to build a new website to support a more youthful image. We not only helped them with their website,' +
         'but also took the opportunity to build them a next generation realtime seat booking app. Contrary to other booking apps, this one works in realtime. If one user clicks a chair, it\'s immediately ' +
         'blocked out in all other users\'s web browsers.',
         tagline: 'Working with cutting edge realtime technology',
@@ -39,12 +38,13 @@ angular.module('proferoSite')
       },
       4: {
         name: 'Gemeente Utrecht',
-        descriptionSmall: 'The municipality of Utrecht hired us to help manage one of their large scale IT projects.',
-        descriptionLarge: '(...)',
-        tagline: 'Making your projects more manageable!',
+        descriptionSmall: 'Implementating software for the municipality of Utrecht.',
+        descriptionLarge: 'We did the functional and technical implementation of a publication system for the public site of the municipality of Utrecht' +
+        'The system, externally hosted and supplied by Cap Gemini / MSI consists of three modules (Babs, BabsOnline and iBabs). It meant proper teamwork was necessary between business and technical project managers, supplier (Cap / MSI), key users, technical specialists and functional managers.',
+        tagline: 'Municipality Management',
         partners: '',
         categories: 'Software, Project Management',
-        image: 'dist/assets/img/bachkoorBooking.jpg',
+        image: 'dist/assets/img/utrecht.jpg',
         link: ''
       }
     });
