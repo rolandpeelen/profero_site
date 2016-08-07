cee4a9b93c0f3372c91e401d56ddd6e71bfb2642
fastf0rward <peterpauloldenzeel@gmail.com>
2016-08-07 13:54:27 +0200

    update project copy

--- src/js/services/ProjectContstant.js
+++ src/js/services/ProjectContstant.js
@@ -5,8 +5,7 @@ angular.module('proferoSite')
       1: {
         name: 'LIPS',
         descriptionSmall: 'Landelijke Inventarisatie Politie Systemen. A software license management system at the Dutch Police.',
-        descriptionLarge: 'In assignment of the Dutch Police we are constantly developing and maintaining the LIPS system. Ever since 2006 the Dutch Police has been using LIPS to consolidate and optimize their wide variety of software packages nation wide. ' +
-        'The Netherlands has as many as 26 distinct Police departments that used to select and license sofware packages autonomously from one another. Using LIPS, license management is now centralized and the Police\'s software wheelpark is getting more efficient every year.' ,
+        descriptionLarge: 'On assignment by the Dutch Police we are constantly developing and maintaining the LIPS system. Ever since 2006 the Dutch Police has been using LIPS to consolidate and optimize their wide variety of software packages nation wide. The Netherlands has as many as 26 distinct Police departments that used to select and license sofware packages autonomously. Using LIPS, license management is now centralized and the Police\'s software wheel park is getting more efficient every year.' ,
         tagline: 'Building durable and scalable software',
         partners: '',
         categories: 'Software, Design',
@@ -16,9 +15,9 @@ angular.module('proferoSite')
       },
       2: {
         name: 'SlimFitted',
-        descriptionSmall: 'An online dress shirt product configurator, enabling clients to compose their next tailored shirt.',
-        descriptionLarge: 'In 2014 we teamed up with the people at SlimFitted to work on a revolutionary new product configurator. Instead of using costly photographs to ' +
-        'show customers a shirt configuration, this configurator uses photorealistic 3d visualisations based on a 3d computer model. Using 3d renders over old-fashioned photographs enables Slimfitted to alter shirt configurations by changing the underlying 3d model and redoing some of the renders, instead of redoing expensive photoshoots.',
+        descriptionSmall: 'An online product configurator for dress shirts.',
+        descriptionLarge: 'In 2014 we worked with the people at SlimFitted to work on their new product configurator. Instead of using costly photographs to ' +
+        'show customers a shirt configuration, this configurator uses photorealistic 3d visualisations based on a 3d computer model. Using 3d renders over old-fashioned photographs enables SlimFitted to alter shirt configurations by changing the underlying 3d model and redoing some of the renders, instead of redoing expensive photoshoots.',
         tagline: 'Developing the next generation of online apps',
         partners: 'Peppr',
         categories: 'Software, Design',
@@ -28,11 +27,9 @@ angular.module('proferoSite')
       },
       3: {
         name: 'Bachkoor Booking',
-        descriptionSmall: 'A realtime booking system showing you the real availability of seats.',
-        descriptionLarge: 'In 2015 \'Bachkoor Nijmegen\' asked us to build their new website to support a more youthful image. We not only helped them with their website,' +
-        'but also took the opportunity to build them a next generation realtime seat booking app. Contrary to other booking apps, this one works in realtime. If one user clicks a chair, it\'s immediately ' +
-        'blocked out in all other users\'s web browsers. No more constantly reloading the page!',
-        tagline: 'Working with cutting edge realtime technology',
+        descriptionSmall: 'A real time booking system showing you the real availability of seats.',
+        descriptionLarge: 'In 2015 the Bachkoor choir in Nijmegen asked us to improve their online booking system. We built them a real time seat booking app. Contrary to other booking apps, this one works in real time. If one user clicks a chair, it is immediately blocked out in all other users\'s web browsers. No more constantly reloading the page!',
+        tagline: 'Working with cutting edge technology',
         partners: '',
         categories: 'Software, Design',
         image: 'dist/assets/img/bachkoorBooking.jpg',
@@ -41,10 +38,8 @@ angular.module('proferoSite')
       },
       4: {
         name: 'Gemeente Utrecht',
-        descriptionSmall: 'Managing a software implementation project for the municipality of Utrecht.',
-        descriptionLarge: 'In 2014 the municipality of Utrecht asked us to manage the technical and functional implementation of a publication system on their public website. ' +
-        'The system consists of three modules (Babs, BabsOnline and iBabs), externally hosted and supplied by Cap Gemini / MSI. The project required intensive team work between business and technical project managers, the supplier (Cap / MSI), key users, technical specialists and functional managers. We made ' +
-        'sure the project went according to plan.',
+        descriptionSmall: 'Managing a key software implementation project for the municipality of Utrecht.',
+        descriptionLarge: 'In 2014 the municipality of Utrecht asked us to manage the technical and functional implementation of a publication system on their public website. The system consists of three modules (Babs, BabsOnline and iBabs), externally hosted and supplied by Cap Gemini / MSI. The project required intensive team work between business and technical project managers, the supplier (Cap / MSI), key users, technical specialists and functional managers. We made sure the project went according to plan.',
         tagline: 'Partner in Interim Management',
         partners: '',
         categories: 'Software, Project Management',
@@ -54,9 +49,8 @@ angular.module('proferoSite')
       },
       5: {
         name: 'Cyber Crime Programme',
-        descriptionSmall: 'Project management and consulting on several crucial Cyber Crime projects.',
-        descriptionLarge: 'In 2010 the Dutch Police requested Profero to help with the project initiation, implementation and management of several IT projects crucial to digital innovations within the Cyber Crime department. ' +
-        'IT innovation is key to the department\'s ability to fight cyber crime. Projects included \'E-mail Analysis\', \'Client-Server\' and \'VMWare ESX\'. ',
+        descriptionSmall: 'Project management and consulting on crucial Cyber Crime projects.',
+        descriptionLarge: 'In 2010 the Dutch Police requested Profero to help with the project initialisation, implementation and management of several IT projects crucial to digital innovations within the Cyber Crime department. IT innovation is key to the department\'s ability to fight cyber crime. Projects included \'Email Analysis\', \'Client-Server\' and \'VMWare ESX\'. ',
         tagline: 'Managing key innovation projects',
         partners: '',
         categories: 'Software, Project Management',
@@ -67,8 +61,7 @@ angular.module('proferoSite')
       6: {
         name: 'Storage Digital Evidence',
         descriptionSmall: 'Evaluating an innovative IT project at the Dutch Police.',
-        descriptionLarge: 'The Dutch Police requested Profero to help with the evaluation of the Storage Digital Evidence project. ' +
-        'We assessed the project\'s process and results by means of PRINCE2 standards.',
+        descriptionLarge: 'The Dutch Police requested Profero to help with the evaluation of the Storage Digital Evidence project. We assessed the project\'s process and results by means of PRINCE2 standards for project management.',
         tagline: 'Assessing project results',
         partners: '',
         categories: 'Software, Project Management, PRINCE2',
